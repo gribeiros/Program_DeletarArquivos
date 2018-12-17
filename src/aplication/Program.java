e67b380431c9ffcd3311def6789430122c08cef3
Gustavo Ribeiro <gustavors071998@gmail.com>
2018-12-17 14:30:41 -0200

    Att 1 teste

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
+		int b = 0;
 		Scanner sc = new Scanner(System.in);
 
 		String local = sc.nextLine();
@@ -16,18 +17,24 @@ public class Program {
 		System.out.print("Deseja excluir arquivo (y/n):");
 		char a = sc.next().charAt(0);
 		try {
-			if (a == 'y') {
-				file.delete();
-				System.out.println("Apagado");
-			} else if (a == 'n') {
-				System.out.println("Finish");
+			while (b == 0) {
+				if (a == 'y') {
+					file.delete();
+					b++;
+					System.out.println("Apagado");
+				} else if (a == 'n') {
+					System.out.println("Finish");
+					b++;
+				} else {
+					System.out.print("Entre com um valor valido:");
+					a = sc.next().charAt(0);
+				}
+
 			}
-			sc.close();
-		} 
-		catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println("Erro: " + e.getMessage());
 		}
-
+		sc.close();
 	}
 
 }
